f6e8c3ec96d2060a800b32d383173e26f30b34f1
grafpoo <grafpoo@gmail.com>
2019-06-21 16:01:47 -0400

    sigh. apt-get needs it's -y

--- scripts/wget-no-log.sh
+++ scripts/wget-no-log.sh
@@ -1,4 +1,4 @@
-sudo apt-get install jq
+sudo apt-get install -y jq
 
 export URL=`curl -s https://api.github.com/repos/rancher/k3s/releases/latest | jq -r ".assets[] | select(.name | test(\"k3s\")) | .browser_download_url" | grep "k3s$"`
 
