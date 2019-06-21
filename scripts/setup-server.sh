fe8e92cfafe5359d60031c3614108309ae6dee3f
grafpoo <grafpoo@gmail.com>
2019-06-21 14:51:41 -0400

    try again

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -3,7 +3,7 @@ export DIR=/srv/scripts
 if [ -e k3s ]; then
   echo "k3s already installed"
 else
-  wget --no-check-certificate https://github.com/rancher/k3s/releases/download/v0.3.0/k3s && \
+  bash $DIR/wget-no-log.sh && \
   sudo cp k3s /usr/local/bin/ && \
   sudo chmod a+x /usr/local/bin/k3s
 fi
---
+++ scripts/wget-no-log.sh
@@ -0,0 +1,1 @@
+wget --no-check-certificate https://github.com/rancher/k3s/releases/download/latest/k3s > /dev/null
