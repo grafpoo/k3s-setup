4af8995f4ccaf3d6673780d4c300ea27e1c09322
grafpoo <grafpoo@gmail.com>
2019-06-21 15:57:12 -0400

    get latest

--- scripts/wget-no-log.sh
+++ scripts/wget-no-log.sh
@@ -1,1 +1,5 @@
-wget --no-check-certificate https://github.com/rancher/k3s/releases/download/latest/k3s > /dev/null
+sudo apt-get install jq
+
+export URL=`curl -s https://api.github.com/repos/rancher/k3s/releases/latest | jq -r ".assets[] | select(.name | test(\"k3s\")) | .browser_download_url" | grep "k3s$"`
+
+wget --no-check-certificate $URL > /dev/null
